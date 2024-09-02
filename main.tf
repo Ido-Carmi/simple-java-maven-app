cc3889bd168fd54d1cd67f692f9bea9e2b11ec84
Ido-Carmi <carmiidoh@gmail.com>
2024-09-02 12:21:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ubuntu" {
-    most_recent = truefilter
-    {
+    most_recent = true
+    filter {
         name   = "name"
         values = ["ubuntu/images/hvm-ssd/*24.04-amd64-server-*"]
     }
