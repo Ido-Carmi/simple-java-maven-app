db547b028d67e883402feff26293a1f2b7ef6b9d
Ido-Carmi <carmiidoh@gmail.com>
2024-09-02 12:11:23 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ubuntu" {
+    most_recent = truefilter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/*24.04-amd64-server-*"]
+    }
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+    owners = ["099720109477"] # Canonical
+}
+# provision to us-east-2 region
+provider "aws" {
+  region  = "ap-northeast-1"
+}
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name      = "keys"
+  tags = {
+    Name = "actions"
+  }
+}
